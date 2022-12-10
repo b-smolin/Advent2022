5b8b90d9a81867be159caa68387a88f4f7582634
Ben Smolin <tud18885@temple.edu>
2022-12-09 19:52:47 -0500

    Day 9 all parts solved

--- Day 9/DayNine.java
+++ Day 9/DayNine.java
@@ -19,74 +19,100 @@ public class DayNine {
         }
     }
 
+//    If the head is ever two steps directly up, down, left, or right from the tail,
+//    the tail must also move one step in that direction so it remains close enough:
+//    Otherwise, if the head and tail aren't touching and aren't in the same row or column,
+//    the tail always moves one step diagonally to keep up:
     public static int partOne(ArrayList<String> moves){
-        Node head = new Node(0,0);
-        Node tail = new Node(0,0);
+        ArrayList<Node> snake = makeSnake(2);
+        Node head = snake.get(0);
+        Node tail = snake.get(1);
         HashSet<String> seen = new HashSet<>();
         for(String move : moves){
             int steps = Integer.parseInt(move.substring(2));
             char direction = move.charAt(0);
             for(int i = 0 ; i < steps ; i++) {
-                int oldX = head.x;
-                int oldY = head.y;
                 switch (direction) {
-                    case 'U':
-                        head.y++;
-                        break;
-                    case 'R':
-                        head.x++;
-                        break;
-                    case 'L':
-                        head.x--;
-                        break;
-                    case 'D':
-                        head.y--;
-                        break;
-                    default:
-                        System.out.println("bad");
-                        break;
+                    case 'U' -> head.y++;
+                    case 'R' -> head.x++;
+                    case 'L' -> head.x--;
+                    case 'D' -> head.y--;
+                    default -> System.out.println("bad");
                 }
-                if(Math.abs(head.x - tail.x) > 1 || Math.abs(head.y - tail.y) > 1){
-                    tail.x = oldX;
-                    tail.y = oldY;
-                }
+                updateSnake(snake);
                 seen.add(tail.toString());
             }
         }
         return seen.size();
     }
 
+    public static ArrayList<Node> makeSnake(int size){
+        ArrayList<Node> snake = new ArrayList<>();
+        for(int i = 0 ; i < size ; i++) {
+            snake.add(new Node(0,0));
+        }
+        return snake;
+    }
+
+    public static void updateSnake(ArrayList<Node> snake){
+        for(int next = 1; next < snake.size() ; next++){
+            if(Math.abs(snake.get(next).x - snake.get(next-1).x) <= 1 && Math.abs(snake.get(next).y - snake.get(next-1).y) <= 1){
+                break;
+            }
+            //current bit follows front bit
+            Node current = snake.get(next);
+            Node front = snake.get(next-1);
+            int xDif = front.x - current.x;
+            int yDif = front.y - current.y;
+
+            if(xDif == -2 && yDif == 0){ //it is two to the left
+                current.x--;
+            }else if(xDif == 2 && yDif == 0){ //it is two to the right
+                current.x++;
+            }
+            else if(xDif == 0 && yDif == 2){ //it is 2 up
+                current.y++;
+            }
+            else if(xDif == 0 && yDif == -2){ //it is 2 down
+                current.y--;
+            }
+            else if(xDif == 2 && yDif == 1 || xDif == 1 && yDif == 2 || xDif == 2 && yDif == 2) {//up right diagonal
+                current.x++;
+                current.y++;
+            }
+            else if(xDif == 2 && yDif == -1 || xDif == 1 && yDif == -2 || xDif == 2 && yDif == -2) {//down right diagonal
+                current.x++;
+                current.y--;
+            }
+            else if(xDif == -2 && yDif == 1 || xDif == -1 && yDif == 2 || xDif == -2 && yDif == 2) {//up left diagonal
+                current.x--;
+                current.y++;
+            }
+            else if(xDif == -2 && yDif == -1 || xDif == -1 && yDif == -2 || xDif == -2 && yDif == -2) {//down left diagonal
+                current.x--;
+                current.y--;
+            }
+        }
+    }
+
     public static int partTwo(ArrayList<String> moves){
         //make same move then adjust if needed?
         HashSet<String> seen = new HashSet<>();
-        ArrayList<Node> rope = new ArrayList<>();
-        for (int i = 0 ; i < 10 ; i++){
-            rope.add(new Node(0,0));
-        }
+        ArrayList<Node> snake = makeSnake(10);
         for(String move: moves) {
             int steps = Integer.parseInt(move.substring(2));
             char direction = move.charAt(0);
-            //go through all the rope bits
+            Node head = snake.get(0);
             for (int i = 0; i < steps; i++) {
-                int oldX = head.x;
-                int oldY = head.y;
                 switch (direction) {
-                    case 'U':
-                        head.y++;
-                        break;
-                    case 'R':
-                        head.x++;
-                        break;
-                    case 'L':
-                        head.x--;
-                        break;
-                    case 'D':
-                        head.y--;
-                        break;
-                    default:
-                        System.out.println("bad");
-                        break;
+                    case 'U' -> head.y++;
+                    case 'R' -> head.x++;
+                    case 'L' -> head.x--;
+                    case 'D' -> head.y--;
+                    default -> System.out.println("bad");
                 }
+                updateSnake(snake);
+                seen.add(snake.get(9).toString());
             }
         }
         return seen.size();
